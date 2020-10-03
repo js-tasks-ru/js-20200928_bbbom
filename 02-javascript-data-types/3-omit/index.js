add07a45dd3a36e37de555c8586900e4055ea3d2
azozulya <bbbom@mail.ru>
2020-10-03 21:01:11 +0300

    Add omit function

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,16 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+  const arr = [...fields];
+  const newObj = [];
 
+  if (Object.keys(obj).length === 0 || arr.length === 0) return {};
+
+  Object.entries(obj).forEach( ([key, val]) => {
+    if (arr.indexOf(key) === -1){
+      newObj.push([key, val]);
+    }
+  });
+
+  return newObj.length > 0 ? Object.fromEntries(newObj) : {};
 };
