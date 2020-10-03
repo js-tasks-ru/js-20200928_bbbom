0801efbbc467265a690423ffedd7e3aca7d2e53d
azozulya <bbbom@mail.ru>
2020-10-03 17:23:55 +0300

    add pick function

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,16 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+  const keys = [...fields];
+  const newObj = [];
 
+  if (Object.keys(obj).length === 0 || keys.length === 0) return {};
+
+  keys.forEach((key) => {
+    if (obj[key]) {
+      newObj.push([key, obj[key]]);
+    }
+  });
+
+  return newObj.length > 0 ? Object.fromEntries(newObj) : {};
 };
