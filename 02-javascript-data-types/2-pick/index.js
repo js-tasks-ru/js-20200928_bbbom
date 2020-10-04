86d66f1c361bc6c885fc51136bad57512ba199f8
azozulya <bbbom@mail.ru>
2020-10-04 21:24:50 +0300

    Add fix for functions(omit, pick)

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -6,15 +6,13 @@
  */
 export const pick = (obj, ...fields) => {
   const keys = [...fields];
-  const newObj = [];
-
-  if (Object.keys(obj).length === 0 || keys.length === 0) return {};
+  const newObj = {};
 
   keys.forEach((key) => {
     if (obj[key]) {
-      newObj.push([key, obj[key]]);
+      newObj[key] = obj[key];
     }
   });
 
-  return newObj.length > 0 ? Object.fromEntries(newObj) : {};
+  return newObj;
 };
--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,16 +5,14 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-  const arr = [...fields];
-  const newObj = [];
+  const keys = [...fields];
+  const newObj = {};
 
-  if (Object.keys(obj).length === 0 || arr.length === 0) return {};
-
-  Object.entries(obj).forEach( ([key, val]) => {
-    if (arr.indexOf(key) === -1){
-      newObj.push([key, val]);
+  Object.entries(obj).forEach(([key, val]) => {
+    if (keys.indexOf(key) === -1) {
+      newObj[key] = val;
     }
   });
 
-  return newObj.length > 0 ? Object.fromEntries(newObj) : {};
+  return newObj;
 };
