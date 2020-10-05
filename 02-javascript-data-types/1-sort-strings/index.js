8879633ccbba03a69f58d82462b685ff09ef8f7d
azozulya <bbbom@mail.ru>
2020-10-05 15:07:51 +0300

    Add sort function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,6 +4,11 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-
+ export function sortStrings(arr, param = 'asc') {
+  return arr.sort((a, b) => {
+    if (param === 'desc') {
+      return b.localeCompare(a, 'ru', {usage: "sort", caseFirst: 'upper'});
+    }
+    return a.localeCompare(b, 'ru', {usage: "sort", caseFirst: 'upper'});
+  });
 }
