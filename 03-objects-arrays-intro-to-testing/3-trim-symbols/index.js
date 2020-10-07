f81cc3b1eec0d4091029493bef5cdb23a37672a9
azozulya <bbbom@mail.ru>
2020-10-07 18:14:37 +0300

    Add trimSymbols function

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,29 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === 0) return "";
 
+  let res = "";
+  const arr = string.split('');
+
+  arr.forEach((letter, idx) => {
+    let flag = false;
+
+    if (idx >= size) {
+      for (let i = idx - 1; i >= idx - size; i--) {
+        flag = (arr[i] === letter);
+      }
+    }
+
+    if (!flag) {
+      res += letter;
+    }
+  });
+  return res;
 }
+//
+// console.log(trimSymbols('xxx', 2)); // 'xx' - удалили один символ
+// console.log(trimSymbols('xxx', 1)); // 'x'
+//
+// console.log(trimSymbols('xxxaaaaabbm', 2));
+// console.log(trimSymbols('xxxaaaaab', 3)); // 'xxxaaab'
