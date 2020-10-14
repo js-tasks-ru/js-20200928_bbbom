a5400c483bfd7723db873055bc389718021c386d
azozulya <bbbom@mail.ru>
2020-10-14 14:50:13 +0300

    Added class NotificationMessage

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,49 @@
-export default class NotificationMessage {
 
+const notifications = document.getElementsByClassName('notification');
+
+export default class NotificationMessage {
+  element;
+
+  constructor(message, {duration = 1000, type = 'success'} = {}){
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+
+  render() {
+    const el = document.createElement('div');
+    el.innerHTML = this.template();
+    this.element = el.firstElementChild;
+  }
+
+  show(target = document.body) {
+    if (notifications.length > 0) {
+      [...notifications].forEach( item => item.remove() );
+    }
+    target.append(this.element);
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">success</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
